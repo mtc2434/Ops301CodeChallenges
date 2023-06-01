658f3034c7d6f8d4d0d60ccf8eee54b83cc277d9
Marcelo <130250660+mtc2434@users.noreply.github.com>
2023-06-01 02:38:48 -0700

    Create appendDandT.sh
    Work in progress

---
+++ appendDandT.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script:  appendDandT.sh                     
+# Author:  Marcelo Clark                      
+# Date of latest revision:   6/1/2023   
+# Purpose:   Create a script that appends the date and time                  
+
+# Declare global variables
+date_currently=$(date +"%Y-%m-%d" ) 
+
+
+# Declare functions
+
+# Main
+echo $date_currently
