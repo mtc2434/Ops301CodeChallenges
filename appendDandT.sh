556b5d732ff60441a1ab1a0107d47e9aec21c209
Marcelo <130250660+mtc2434@users.noreply.github.com>
2023-06-01 21:49:53 -0700

    Update appendDandT.sh
    updated and finished code challenge

--- appendDandT.sh
+++ appendDandT.sh
@@ -6,10 +6,12 @@
 # Purpose:   Create a script that appends the date and time                  
 
 # Declare global variables
-date_currently=$(date +"%Y-%m-%d" ) 
+
 
 
 # Declare functions
 
 # Main
-echo $date_currently
+
+# copies the date and time to a log file, and creates the log file itself and the name
+cp /var/log/syslog "syslog_$(date +"%Y-%m-%d")"
