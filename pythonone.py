bc96135cb2b08add14bb7bd5459f73d9c421da9c
Marcelo <130250660+mtc2434@users.noreply.github.com>
2023-06-07 07:44:41 -0700

    Create pythonone.py
    work in progress

---
+++ pythonone.py
@@ -0,0 +1,16 @@
+import os
+
+# Script Name:                  pythonone.py
+# Author:                       Marcelo Clark
+# Date of latest revision:      06/07/2023
+# Purpose:                      The Python module “os” must be utilized at least three variables must be declared and referenced in Python
+#                               
+# Declaration of variables
+#creates whoami variable
+whoami_output = os.popen('whoami').read().strip()
+
+
+# Declaration of functions
+
+
+# Main
