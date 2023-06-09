6fdf65f90e8b4de4c572957001e52e5825660b00
Marcelo <130250660+mtc2434@users.noreply.github.com>
2023-06-09 08:33:52 -0700

    Create string_galore.py

---
+++ string_galore.py
@@ -0,0 +1,16 @@
+# # #!/usr/bin/env python3
+
+
+#codename: string_galore.py
+#Task Create a Python script that includes the following:
+#author: Marcelo Clark
+#Date of last revision: 6/9/2023
+
+# Assign to a variable a list of ten string elements.
+# Print the fourth element of the list.
+# Print the sixth through tenth element of the list.
+# Change the value of the seventh element to “onion”.
+
+
+#Main
+shopping_list = ["apple", "banana", "cherry", "marionberry", "blackberry", "mango", "grape", "honeydew", "dragonfruit", "pineapple"]
