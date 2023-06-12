5a3c33bb54ee65e99a5a59897e2ae52a6b279877
Marcelo <130250660+mtc2434@users.noreply.github.com>
2023-06-12 08:33:29 -0700

    Create equalornot.py

---
+++ equalornot.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Import libraries
+
+import os
+
+# Script Name:                  equalornot.py
+# Author:                       Marcelo Clark
+# Date of latest revision:      6/12/2023
+# Purpose:                      Purpose
+
+# Declaration of variables
+a = 1
+b = 2
+
+### Read user input here into a variable
+
+# Declaration of functions
+
+# Main
+# checks if a is equal to b and will print "a is equal to b" if this is true
+if a == b:
+    print("a is equal to b")
+
+
+# End
