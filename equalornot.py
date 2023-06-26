ec1eb165a15a070351d2ecd9b14d70e2e83ca07e
Marcelo <130250660+mtc2434@users.noreply.github.com>
2023-06-25 21:42:43 -0700

    Update equalornot.py

--- equalornot.py
+++ equalornot.py
@@ -6,7 +6,7 @@ import os
 
 # Script Name:                  equalornot.py
 # Author:                       Marcelo Clark
-# Date of latest revision:      6/12/2023
+# Date of latest revision:      6/25/2023
 # Purpose:                      Purpose
 
 # Declaration of variables
@@ -21,6 +21,16 @@ b = 2
 # checks if a is equal to b and will print "a is equal to b" if this is true
 if a == b:
     print("a is equal to b")
+    elif a < b:
+    print("a is less than b")
+elif a <= b:
+    print("a is less than or equal to b")
+elif a > b:
+    print("a is greater than b")
+elif a >= b:
+    print("a is greater than or equal to b")
+else:
+    print("Something really bad happened")
 
 
 # End
