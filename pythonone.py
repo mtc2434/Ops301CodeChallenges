6af1a04c49b27670ed482147381cbb505a9fa616
Marcelo <130250660+mtc2434@users.noreply.github.com>
2023-06-25 21:37:13 -0700

    Update pythonone.py

--- pythonone.py
+++ pythonone.py
@@ -4,13 +4,27 @@ import os
 # Author:                       Marcelo Clark
 # Date of latest revision:      06/07/2023
 # Purpose:                      The Python module “os” must be utilized at least three variables must be declared and referenced in Python
-#                               
-# Declaration of variables
+                               
+
+
+#Main
 #creates whoami variable
 whoami_output = os.popen('whoami').read().strip()
 
+# Executes ip a command an puts output into variable
+ip_output = os.popen('ip a').read().strip()
 
-# Declaration of functions
+# puts output of 'lshw -short' into a variable
+lshw_output = os.popen('lshw -short').read().strip()
+
+# prints the values of the variables
+print("Current user:", whoami_output)
+print("IP addresses:", ip_output)
+print("System hardware information:", lshw_output)
+
+#End
 
 
-# Main
+
+
+
